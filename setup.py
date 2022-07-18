4bb55658fcb7fdfbd5ccd715944fba8bb057d9cd
YasunoriHirakawa <area51muepon1ichi@gmail.com>
2022-07-18 18:47:39 +0900

    Fix url

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     license="MIT",
     description="Convenience wrapper for configuring ArgumentParser using a class.",
     author="Yasunori Hirakawa",
-    url="https://github.com/YasunoriHirakawa/smartargparse.git",
+    url="https://github.com/YasunoriHirakawa/smartargparse.git#egg=smartargparse",
     package=find_packages(),
     install_requires=[]
 )
